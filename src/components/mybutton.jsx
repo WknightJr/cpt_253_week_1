0c20a7dd1fafe1ddb938ae36b1ec204143e56a32
WknightJr <austinbouchard6@gmail.com>
2025-01-25 17:47:24 -0500

    Update mybutton.jsx

--- src/components/mybutton.jsx
+++ src/components/mybutton.jsx
@@ -1,5 +1,11 @@
 function MyButton() {
     return (
-      <button>Hello World!</button>
+      <button onclick="showAlert()">Hello World!</button>
     );
   }
+  <script>
+  function showAlert() {
+    var myText = "Hello World!";
+    alert (myText);
+  }
+  </script>
