54adf0694da067e27436d31c930d16a963e772c5
WknightJr <austinbouchard6@gmail.com>
2025-01-25 17:49:31 -0500

    Update mybutton.jsx

--- src/components/mybutton.jsx
+++ src/components/mybutton.jsx
@@ -3,9 +3,13 @@ function MyButton() {
       <button onclick="showAlert()">Hello World!</button>
     );
   }
+//This is the button. What I am hoping it does is both say Hello World and give a message when clicked.//
+
+
   <script>
   function showAlert() {
     var myText = "Hello World!";
     alert (myText);
   }
   </script>
+//This is the message text. I found this online. I have no idea if it is doing anything or not.//
