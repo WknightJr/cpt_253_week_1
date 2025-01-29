81927a69341288e684e5514f7bfc367458287d58
WknightJr <austinbouchard6@gmail.com>
2025-01-29 15:49:22 -0500

    Update mybutton.jsx

--- src/components/mybutton.jsx
+++ src/components/mybutton.jsx
@@ -5,11 +5,15 @@ function MyButton() {
   }
 //This is the button. What I am hoping it does is both say Hello World and give a message when clicked.//
 
+export const MyButton = () =>{
 
-  <script>
-  function showAlert() {
-    var myText = "Hello World!";
-    alert (myText);
-  }
-  </script>
-//This is the message text. I found this online. I have no idea if it is doing anything or not.//
+    const sayHelloWorld = () =>{
+        alert('Hello World!');
+    }
+
+return (
+    <button onClick={ () => sayHelloWorld()}>Say Hi!</button>
+    )
+}
+        
+//This is the message text.//
